90e87bb0ddbff3a6c5e33b9488c4937cac39961b
Jeremy Herwig <jherwig1@vols.utk.edu>
2018-03-31 15:19:12 -0400

    Add error handling

--- kilo.c
+++ kilo.c
@@ -1,37 +1,67 @@
+/*** includes ***/
 #include <termios.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <errno.h>
 
+
+/*** data ***/
 struct termios orig_termios;
 
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
+/*** terminal **/
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+		die("tcsetattr");
+	}
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+		die("tcgetattr");
+	}
+
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+	raw.c_cc[VMIN] = 0; // min chars for read
+	raw.c_cc[VTIME] = 1; // timeout, tenths of second
+
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die("tcsetattr");
+	}
 }
 
+/*** init ***/
 int main() {
 	enableRawMode();
-
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+	while (1) {
+		c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			die("read");
+		}
+
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		}
 		 else {
 			printf("%d ('%c')\r\n", c, c);
 		}
+
+		if (c == 'q') return 0;
 	}
 	return 0;
 }
